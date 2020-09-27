6289068b1c4dbb88017f20b68282b663865f5d76
Dmitry <dima.matatoff@yandex.ru>
2020-09-27 22:15:50 +0300

    Rename app

--- android/app/src/main/java/com/astronomy/MainActivity.java
+++ android/app/src/main/java/com/astronomy/MainActivity.java
@@ -1,4 +1,4 @@
-package com.navigationapp;
+package com.astronomy;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "NavigationApp";
+    return "Astronomy";
   }
 }
--- ios/AstronomyTests/AstronomyTests.m
+++ ios/AstronomyTests/AstronomyTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface NavigationAppTests : XCTestCase
+@interface AstronomyTests : XCTestCase
 
 @end
 
-@implementation NavigationAppTests
+@implementation AstronomyTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
