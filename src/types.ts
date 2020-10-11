0ea2a7f06483551f392eeca199298d842cce2a46
Dmitry <dima.matatoff@yandex.ru>
2020-10-11 13:16:28 +0300

    Remove junk types

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,5 @@
-import { FormikHelpers } from 'formik'
-
 import { AuthReducer } from './constants'
-import { MutationLoginArgs, RegisterInput, User } from './generated/graphql'
-
-//Formik
-export type FormikOnSubmit<T> = (values: T, formikHelpers: FormikHelpers<T>) => void | Promise<any>
-
-export type TSignUpFormik = FormikOnSubmit<RegisterInput>
-export type TSignInFormik = FormikOnSubmit<MutationLoginArgs>
+import { User } from './generated/graphql'
 
 //useCurrentUser hook
 export type TAuthReducer = { type: AuthReducer.Login; payload: User } | { type: AuthReducer.Logout }
