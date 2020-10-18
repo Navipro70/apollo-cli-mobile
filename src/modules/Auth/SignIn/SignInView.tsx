523a557f8e0256350db0584f8d77a9f807309d4a
Dmitry <dima.matatoff@yandex.ru>
2020-10-18 21:17:31 +0300

    Remove unnes import

--- src/modules/Auth/SignIn/SignInView.tsx
+++ src/modules/Auth/SignIn/SignInView.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import { StyleSheet } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import { SafeAreaView } from 'react-native-safe-area-context'
